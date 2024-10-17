b9f82eb3fea31df17612410f31c5e14b8d57a6f7
nandhagk <nandhagopi@gmail.com>
2024-10-17 21:20:48 +0530

    refactor rectangle_union

--- lib/rectangle_union.hpp
+++ lib/rectangle_union.hpp
@@ -11,62 +11,57 @@
 #include <lib/lazy_segment_tree.hpp>
 
 template <typename T>
-struct rectangle_union {
-	std::vector<T> xs, ys;
+T rectangle_union(const std::vector<std::array<T, 4>> &rs) {
+	// x0, y0, x1, y1
+	const i32 n = static_cast<i32>(rs.size()) * 2;
 
-	void add(T xl, T yt, T xr, T yb) {
-		xs.push_back(xl);
-		xs.push_back(xr);
-
-		ys.push_back(yt);
-		ys.push_back(yb);
+	std::vector<T> xs(n), ys(n);
+	for (i32 i = 0; i < n; ++i) {
+		xs[i] = rs[i / 2][0 + 2 * (i % 2)];
+		ys[i] = rs[i / 2][1 + 2 * (i % 2)];
 	}
 
-	T area() {
-		const i32 n = static_cast<i32>(xs.size());
+	std::vector<i32> xi(n), yi(n), yr(n);
+	std::iota(xi.begin(), xi.end(), 0);
+	std::iota(yi.begin(), yi.end(), 0);
 
-		std::vector<i32> xi(n), yi(n), yr(n);
-		std::iota(xi.begin(), xi.end(), 0);
-		std::iota(yi.begin(), yi.end(), 0);
+	std::sort(xi.begin(), xi.end(), [&](const i32 a, const i32 b) { 
+		return xs[a] == xs[b] ? a < b : xs[a] < xs[b]; 
+	});
 
-		std::sort(xi.begin(), xi.end(), [&](const i32 a, const i32 b) { 
-			return xs[a] == xs[b] ? a < b : xs[a] < xs[b]; 
-		});
+	std::sort(yi.begin(), yi.end(), [&](const i32 a, const i32 b) { 
+		return ys[a] == ys[b] ? a < b : ys[a] < ys[b]; 
+	});
 
-		std::sort(yi.begin(), yi.end(), [&](const i32 a, const i32 b) { 
-			return ys[a] == ys[b] ? a < b : ys[a] < ys[b]; 
-		});
-
-		std::vector<T> as(n), bs(n);
-		for (i32 i = 0; i < n; ++i) {
-			as[i] = xs[xi[i]];
-			bs[i] = ys[yi[i]];
-			yr[yi[i]] = i;
-		}
-
-		lazy_segment_tree<acted_monoid_min_cnt_add<i32, T>> st(n - 1, [&](const i32 i) -> std::pair<i32, T> { 
-			return {0, bs[i + 1] - bs[i]}; 
-		});
-
-		T area{};
-		T total = bs[n - 1] - bs[0];
-
-		for (i32 i = 0; i < n - 1; ++i) {
-			const i32 k = xi[i] / 2;
-			const i32 a = xi[i] % 2 ? -1 : 1;
-
-			const auto [l, r] = std::minmax(yr[2 * k], yr[2 * k + 1]);
-			st.apply(l, r, a);
-
-			const auto [m, c] = st.prod_all();
-			const T dy = total - (m == 0 ? c : 0);
-			const T dx = as[i + 1] - as[i];
-
-			area += dx * dy;
-		}
-
-		return area;
+	std::vector<T> as(n), bs(n);
+	for (i32 i = 0; i < n; ++i) {
+		as[i] = xs[xi[i]];
+		bs[i] = ys[yi[i]];
+		yr[yi[i]] = i;
 	}
+
+	lazy_segment_tree<acted_monoid_min_cnt_add<i32, T>> st(n - 1, 
+		[&](const i32 i) -> std::pair<i32, T> { return {0, bs[i + 1] - bs[i]}; 	});
+
+	T area{};
+	T total = bs[n - 1] - bs[0];
+
+	for (i32 i = 0; i < n - 1; ++i) {
+		const i32 k = xi[i] / 2;
+		const i32 a = xi[i] % 2 ? -1 : 1;
+
+		const auto [l, r] = std::minmax(yr[2 * k], yr[2 * k + 1]);
+		st.apply(l, r, a);
+
+		const auto [m, c] = st.prod_all();
+
+		const T dy = total - (m == 0 ? c : 0);
+		const T dx = as[i + 1] - as[i];
+
+		area += dx * dy;
+	}
+
+	return area;
 };
 
 #endif // LIB_RECTANGLE_UNION_HPP
