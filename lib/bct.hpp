b477d14f3b191e4b43ccdf33dcea5f41237278cc
nandhagk <nandhagopi@gmail.com>
2024-10-26 13:13:57 +0530

    add bct

---
+++ lib/bct.hpp
@@ -0,0 +1,65 @@
+#ifndef LIB_BCT_HPP
+#define LIB_BCT_HPP 1
+
+#include <vector>
+#include <lib/prelude.hpp>
+
+inline std::pair<std::vector<bool>, std::vector<std::vector<i32>>> bct(const std::vector<std::vector<i32>> &g) {
+	const i32 n = static_cast<i32>(g.size());
+
+        std::vector<i32> seen, tin(n, -1), low(n);
+        seen.reserve(n);
+
+        std::vector<std::vector<i32>> ccs;
+        std::vector<bool> c(n, false);
+
+        i32 time{};
+        const auto dfs = [&](auto &&self, i32 u, i32 t = -1) -> void {
+                seen.push_back(u);
+                tin[u] = low[u] = time++;
+ 
+                i32 child{};
+                for (const i32 v : g[u]) {
+                        if (v == t) continue;
+
+                        if (tin[v] == -1) {
+                                ++child;
+                                const i32 s = static_cast<i32>(seen.size());
+
+                                self(self, v, u);
+                                low[u] = std::min(low[u], low[v]);
+
+                                if ((t == -1 && child > 1) || (t != -1 && low[v] >= tin[u])) {
+                                        c[u] = true;
+                                        auto &cc = ccs.back();
+ 
+                                        cc.push_back(u);
+                                        while (static_cast<i32>(seen.size()) > s) {
+                                                cc.push_back(seen.back());
+                                                seen.pop_back();
+                                        }
+ 
+                                        ccs.emplace_back();
+                                }
+                        } else {
+                                low[u] = std::min(low[u], tin[v]);
+			}
+                }
+        };
+ 
+        for (i32 u = 0; u < n; ++u) {
+                if (tin[u] != -1) continue;
+ 
+                ccs.emplace_back();
+                dfs(dfs, u);
+ 
+                auto &cc = ccs.back();
+                for (const i32 v : seen) cc.push_back(v);
+ 
+                seen.clear();
+        }
+
+	return {std::move(c), std::move(ccs)};
+}
+
+#endif // LIB_BCT_HPP
