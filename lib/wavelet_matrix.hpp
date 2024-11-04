540582869b1179c52012aa863bd39b1480f36593
nandhagk <nandhagopi@gmail.com>
2024-11-04 14:57:14 +0530

    update wavelet

--- lib/wavelet_matrix.hpp
+++ lib/wavelet_matrix.hpp
@@ -169,6 +169,14 @@ struct wavelet_matrix {
 
                 return rv[p];
         }
+
+        T next(i32 l, i32 r, T a) const {
+                const auto it = std::upper_bound(rv.begin(), rv.end(), a);
+                if (it == rv.end()) return a;
+
+                const i32 k = count(l, r, *it);
+                return k == r - l ? a : kth(l, r, k);
+        }
 };
 
 template <typename T>
@@ -263,6 +271,38 @@ struct hld_wavelet_matrix {
 	T kth_subtree(i32 u, i32 k) const {
 		return wm.kth(h.tin[u], h.tin[u] + h.sz[u], k);
 	}
+
+        T next_path(i32 u, i32 v, T a) const {
+                const auto it = std::upper_bound(wm.rv.begin(), wm.rv.end(), a);
+                if (it == wm.rv.end()) return a;
+
+                const i32 k = count_path(u, v, *it);
+                return k == h.dist(u, v) + 1 ? a : kth_path(u, v, k);
+        }
+
+        T prev_path(i32 u, i32 v, T a) const {
+                const auto it = std::lower_bound(wm.rv.begin(), wm.rv.end(), a);
+                if (it == wm.rv.begin()) return a;
+
+                const i32 k = count_path(u, v, *std::prev(it));
+                return k == 0 ? a : kth_path(u, v, k);
+        }
+
+        T next_subtree(i32 u, T a) const {
+                const auto it = std::upper_bound(wm.rv.begin(), wm.rv.end(), a);
+                if (it == wm.rv.end()) return a;
+
+                const i32 k = count_subtree(u, *it);
+                return k == h.sz[u] ? a : kth_subtree(u, k);
+        }
+
+        T prev_subtree(i32 u, T a) const {
+                const auto it = std::lower_bound(wm.rv.begin(), wm.rv.end(), a);
+                if (it == wm.rv.begin()) return a;
+
+                const i32 k = count_path(u, *std::prev(it));
+                return k == 0 ? a : kth_path(u, k);
+        }
 };
 
 #endif // LIB_WAVELET_MATRIX_HPP
