db1d8a10bf7991a90d277f79d419c86f20019599
nandhagk <nandhagopi@gmail.com>
2024-11-19 13:18:17 +0530

    fix scc

--- contest/a/a.cpp
+++ contest/a/a.cpp
@@ -2,18 +2,35 @@
 #include <contest/debug.hpp>
 
 #include <lib/prelude.hpp>
+#include <lib/scc.hpp>
 
 void solve() {
+        i32 n, m;
+        std::cin >> n >> m;
+ 
+        std::vector<std::vector<i32>> g(n);
+        while (m--) {
+                i32 u, v;
+                std::cin >> u >> v;
+ 
+                --u;
+                --v;
+ 
+                g[u].push_back(v);
+        }
+ 
+        const auto ids = scc(g);
+        const auto es = make_strongly_connected(g, ids);
+ 
+        std::cout << static_cast<i32>(es.size()) << '\n';
+        for (const auto &[u, v] : es) {
+                std::cout << u + 1 << ' ' << v + 1 << '\n';
+        }
 }
 
 i32 main() {
         std::ios_base::sync_with_stdio(false);
         std::cin.tie(nullptr);
 
-        i32 t;
-        std::cin >> t;
-
-        while (t--) {
-                solve();
-        }
+        solve();
 }
