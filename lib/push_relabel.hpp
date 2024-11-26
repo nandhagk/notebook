461290543cae392b79bbe786e18f338f52788a37
nandhagk <nandhagopi@gmail.com>
2024-11-26 15:51:44 +0530

    add push_relabel

--- lib/push_relabel.hpp
+++ lib/push_relabel.hpp
@@ -7,113 +7,90 @@
 
 #include <lib/prelude.hpp>
 
-template <typename T> 
-struct push_relabel {
-        struct edge {
-                i32 from, to;
-                T cap, flow;
-                i32 nxt;
+template <typename T>
+inline std::vector<T> push_relabel(const std::vector<std::vector<std::pair<i32, T>>> &g, i32 s, i32 t) {
+        const i32 n = static_cast<i32>(g.size());
 
-                edge(i32 u, i32 v, T c, i32 nx): 
-                        from(u), to(v), cap(c), flow(0), nxt(nx) {}
+        i32 k{};
+        std::vector<std::vector<std::tuple<i32, i32, T>>> h(n);
+        for (i32 u = 0; u < n; ++u) {
+                for (const auto &[v, cap] : g[u]) {
+                        h[u].emplace_back(v, k++, cap);
+                        h[v].emplace_back(u, k++, 0);
+                }
+        }
+
+        std::vector<T> flow(k), excess(n);
+        std::vector<i32> lvl(n), cnt(n + 1);
+        std::vector<std::vector<i32>> lst(n);
+        std::vector<bool> active(n, false);
+
+        i32 max{};
+        for (const auto &[_, cap] : g[s]) excess[s] += cap;
+
+        const auto enqueue = [&](i32 u) -> void {
+                if (active[u] || excess[u] == 0 || lvl[u] == n) return;
+
+                active[u] = true;
+                lst[lvl[u]].push_back(u);
+                max = std::max(max, lvl[u]);
         };
 
-        i32 n, max_level;
-        std::vector<edge> es;
-        std::vector<std::vector<i32>> lst;
-        std::vector<i32> head, level, cnt;
-        std::vector<T> excess;
-        std::vector<bool> active;
+        cnt[0] = n;
+        active[t] = true;
 
-        explicit push_relabel(i32 m) {
-                build(m);
-        }
+        enqueue(s);
+        while (max >= 0) {
+                if (lst[max].empty()) {
+                        --max;
+                        continue;
+                }
 
-        void build(i32 m) {
-                n = m;
-                lst.resize(n);
-                head.assign(n, -1);
-        }
+                const i32 u = lst[max].back();
+                lst[max].pop_back();
 
-        void add_edge(i32 from, i32 to, T cap) {
-                const i32 id = static_cast<i32>(es.size());
-                es.emplace_back(from, to, cap, head[from]);
-                head[from] = id;
-                es.emplace_back(to, from, 0, head[to]);
-                head[to] = id + 1;
-        }
+                active[u] = false;
+                for (const auto &[v, i, cap] : h[u]) {
+                        const auto d = std::min(excess[u], cap - flow[i]);
+                        if (d != 0 && lvl[u] == lvl[v] + 1) {
+                                flow[i] += d;
+                                flow[i ^ 1] -= d;
 
-        T max_flow(i32 s, i32 t) {
-                for (auto &&e : es) e.flow = 0;
-                for (auto &&l : lst) l.clear();
+                                excess[v] += d;
+                                excess[u] -= d;
 
-                max_level = 0;
-                level.assign(n, 0);
-                excess.assign(n, 0);
-                cnt.assign(n + 1, 0);
-                active.assign(n, false);
-
-                for (i32 i = head[s]; i != -1; i = es[i].nxt) excess[s] += es[i].cap;
-                cnt[0] = n;
-                enqueue(s);
-                active[t] = true;
-                while (max_level >= 0) {
-                        if (lst[max_level].empty()) {
-                                --max_level;
-                                continue;
-                        }
-
-                        const i32 u = lst[max_level].back();
-                        lst[max_level].pop_back();
-
-                        active[u] = false;
-                        for (i32 i = head[u]; i != -1 && excess[u] > 0; i = es[i].nxt) {
-                                const auto d = std::min(excess[u], es[i].cap - es[i].flow);
-                                if (d > 0 && level[u] == level[es[i].to] + 1) {
-                                        es[i].flow += d; es[i ^ 1].flow -= d;
-                                        excess[es[i].to] += d; excess[u] -= d;
-                                        enqueue(es[i].to);
-                                }
-                        }
-
-                        if (excess[u] == 0) continue;
-
-                        if (cnt[level[u]] == 1) {
-                                const i32 k = level[u];
-                                for (i32 v = 0; v < n; ++v) {
-                                        if (level[v] < k) continue;
-
-                                        --cnt[level[v]];
-                                        level[v] = n;
-
-                                        ++cnt[level[v]];
-                                        enqueue(v);
-                                }
-                        } else {
-                                --cnt[level[u]];
-                                level[u] = n;
-
-                                for (i32 i = head[u]; i != -1; i = es[i].nxt) {
-                                        if (es[i].cap == es[i].flow) continue;
-                                        level[u] = std::min(level[u], level[es[i].to] + 1);
-                                }
-
-                                ++cnt[level[u]];
-                                enqueue(u);
+                                enqueue(v);
                         }
                 }
 
-                return excess[t];
-        }
+                if (excess[u] == 0) continue;
 
-private:
-        void enqueue(i32 u) {
-                if (!active[u] && excess[u] > 0 && level[u] < n) {
-                        active[u] = true;
-                        lst[level[u]].push_back(u);
-                        max_level = std::max(max_level, level[u]);
+                if (cnt[lvl[u]] == 1) {
+                        const i32 l = lvl[u];
+                        for (i32 v = 0; v < n; ++v) {
+                                if (lvl[v] < l) continue;
+
+                                --cnt[lvl[v]];
+                                lvl[v] = n;
+
+                                ++cnt[lvl[v]];
+                                enqueue(v);
+                        }
+                } else {
+                        --cnt[lvl[u]];
+                        lvl[u] = n;
+
+                        for (const auto &[v, i, cap] : h[u]) {
+                                if (cap == flow[i]) continue;
+                                lvl[u] = std::min(lvl[u], lvl[v] + 1);
+                        }
+
+                        ++cnt[lvl[u]];
+                        enqueue(u);
                 }
         }
+
+        return excess;
 };
 
 #endif // LIB_PUSH_RELABEL_HPP
