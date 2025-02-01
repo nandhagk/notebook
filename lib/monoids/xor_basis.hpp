928831d3721bc9fce20ffee8133d43e4ada91650
nandhagk <nandhagopi@gmail.com>
2025-02-01 15:16:24 +0530

    add monoid xor basis

---
+++ lib/monoids/xor_basis.hpp
@@ -0,0 +1,56 @@
+#ifndef LIB_MONOID_XOR_BASIS_HPP
+#define LIB_MONOID_XOR_BASIS_HPP 1
+
+#include <limits>
+#include <array>
+
+#include <lib/prelude.hpp>
+#include <lib/type_traits.hpp>
+
+template <typename T, i32 W = std::numeric_limits<T>::digits, is_integral_t<T> * = nullptr>
+struct monoid_xor_basis {
+    using X = std::pair<i32, std::array<T, W>>;
+    using ValueT = X;
+
+    static constexpr X op(const X& x, const X& y) {
+        X z = x;
+
+        const auto &[sz, b] = y;
+        for (i32 i = 0; i < W; ++i)
+            insert(z, b[i]);
+
+        return z;
+    }
+
+    static constexpr X unit() {
+        return {0, std::array<T, W>{}};
+    }
+
+    static constexpr X from_element(T t) {
+        X x = unit();
+        insert(x, t);
+
+        return x;
+    }
+
+    static constexpr void insert(X& x, T t) {
+        auto &[sz, b] = x;
+        for (i32 i = 0; i < W; ++i) {
+            if ((t >> i) & 1) {
+                if (!b[i]) {
+                    b[i] = t;
+                    ++sz;
+
+                    return;
+                }
+
+                t ^= b[i];
+            }
+        }
+    }
+
+    static constexpr bool commutative = true;
+};
+
+#endif // LIB_MONOID_XOR_BASIS_HPP
+
