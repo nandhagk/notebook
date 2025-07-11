3e4cfd7cc5237a3f270412832f2f5314320ed0ad
nandhagk <nandhagopi@gmail.com>
2025-07-11 20:38:53 +0530

    make bitset better

--- lib/bitset.hpp
+++ lib/bitset.hpp
@@ -1,235 +1,270 @@
 #ifndef LIB_BITSET_HPP
 #define LIB_BITSET_HPP 1
 
+#include <algorithm>
 #include <array>
 #include <functional>
 #include <iostream>
 #include <limits>
+#include <string>
 
 #include <lib/bits.hpp>
 #include <lib/prelude.hpp>
 
-template <usize N, typename T = u128>
-class bitset {
-public:
-    static constexpr usize W = std::numeric_limits<T>::digits;
-    static_assert((W & (W - 1)) == 0, "sizeof(T) must be a power of 2");
+template <usize N, typename W, bool D, typename E>
+class expr {
+protected:
+    static constexpr usize S = std::numeric_limits<W>::digits;
+    static constexpr usize M = (N + S - 1) / S;
 
-    static constexpr usize S = topbit(W);
-    static constexpr usize M = W - 1;
-    static constexpr usize B = (N + W - 1) >> S;
+public:
+    static constexpr bool dir = D;
+
+    [[gnu::always_inline, nodiscard]] constexpr const E &self() const {
+        return *static_cast<const E *>(this);
+    }
+
+    [[gnu::always_inline, nodiscard]] constexpr W word(usize pos) const {
+        return self().word(pos);
+    }
+
+    [[gnu::always_inline, nodiscard]] constexpr usize count() const {
+        usize result = 0;
+
+        for (usize i = 0; i < M; ++i) result += popcnt(word(i));
+        return result;
+    }
+
+    [[gnu::always_inline, nodiscard]] constexpr bool operator[](usize pos) const {
+        return (word(pos / S) >> (pos % S)) & 1;
+    }
+};
+
+template <usize N, typename W, typename Op, typename L, typename R>
+class binary_expr : public expr<N, W, R::dir, binary_expr<N, W, Op, L, R>> {
+    const L &lhs;
+    const R &rhs;
+    [[no_unique_address]] Op op;
+
+public:
+    constexpr binary_expr(const L &l, const R &r)
+        : lhs(l), rhs(r) {}
+
+    [[gnu::always_inline, nodiscard]] constexpr W word(usize pos) const {
+        return op(lhs.word(pos), rhs.word(pos));
+    }
+};
+
+template <usize N, typename W, typename E>
+class shl_expr : public expr<N, W, false, shl_expr<N, W, E>> {
+    using base_t = expr<N, W, false, shl_expr<N, W, E>>;
+
+    using base_t::M;
+    using base_t::S;
+
+    const E &lhs;
+    const usize wshift, offset, sub_offset;
+
+public:
+    constexpr shl_expr(const E &e, usize shift)
+        : lhs(e), wshift(shift / S), offset(shift % S), sub_offset(S - offset) {}
+
+    [[gnu::always_inline, nodiscard]] constexpr W word(usize pos) const {
+        if (pos < wshift) return 0;
+        if (offset == 0) return lhs.word(pos - wshift);
+
+        if (pos == wshift) return lhs.word(0) << offset;
+        return (lhs.word(pos - wshift) << offset) | (lhs.word(pos - wshift - 1) >> sub_offset);
+    }
+};
+
+template <usize N, typename W, typename E>
+struct shr_expr : public expr<N, W, true, shl_expr<N, W, E>> {
+    using base_t = expr<N, W, true, shl_expr<N, W, E>>;
+
+    using base_t::M;
+    using base_t::S;
+
+    const E &lhs;
+    const usize wshift, offset, sub_offset, limit;
+
+public:
+    constexpr shr_expr(const E &e, usize shift)
+        : lhs(e), wshift(shift / S), offset(shift % S), sub_offset(S - offset), limit(M - wshift - 1) {}
+
+    [[gnu::always_inline, nodiscard]] constexpr W word(usize pos) const {
+        if (pos > limit) return 0;
+        if (offset == 0) return lhs.word(pos + wshift);
+
+        if (pos == limit) return lhs.word(M - 1) >> offset;
+        return (lhs.word(pos + wshift) >> offset) | (lhs.word(pos + wshift + 1) << (sub_offset));
+    }
+};
+
+template <usize N, typename W, typename L, typename R, bool D1, bool D2>
+[[gnu::always_inline, nodiscard]] constexpr auto operator|(const expr<N, W, D1, L> &lhs, const expr<N, W, D2, R> &rhs) {
+    return binary_expr<N, W, std::bit_or<W>, L, R>(lhs.self(), rhs.self());
+}
+
+template <usize N, typename W, typename L, typename R, bool D1, bool D2>
+[[gnu::always_inline, nodiscard]] constexpr auto operator&(const expr<N, W, D1, L> &lhs, const expr<N, W, D2, R> &rhs) {
+    return binary_expr<N, W, std::bit_and<W>, L, R>(lhs.self(), rhs.self());
+}
+
+template <usize N, typename W, typename L, typename R, bool D1, bool D2>
+[[gnu::always_inline, nodiscard]] constexpr auto operator^(const expr<N, W, D1, L> &lhs, const expr<N, W, D2, R> &rhs) {
+    return binary_expr<N, W, std::bit_xor<W>, L, R>(lhs.self(), rhs.self());
+}
+
+template <usize N, typename W, typename E, bool D>
+[[gnu::always_inline, nodiscard]] constexpr auto operator<<(const expr<N, W, D, E> &lhs, usize shift) {
+    return shl_expr<N, W, E>(lhs.self(), shift);
+}
+
+template <usize N, typename W, typename E, bool D>
+[[gnu::always_inline, nodiscard]] constexpr auto operator>>(const expr<N, W, D, E> &lhs, usize shift) {
+    return shr_expr<N, W, E>(lhs.self(), shift);
+}
+
+template <usize N, typename W = u128>
+class bitset : public expr<N, W, true, bitset<N, W>> {
+    using base_t = expr<N, W, true, bitset<N, W>>;
+
+    using base_t::M;
+    using base_t::S;
+
+    std::array<W, M> d{};
+
+public:
+    constexpr bitset() = default;
+    constexpr bitset(const bitset &other) = default;
+    constexpr bitset(bitset &&other) = default;
 
     class reference {
     public:
         [[gnu::always_inline]] constexpr reference &operator=(bool val) {
             if (val)
-                *word_ |= T(1) << pos_;
+                *word_ptr |= static_cast<W>(1) << pos;
             else
-                *word_ &= ~(T(1) << pos_);
+                *word_ptr &= ~(static_cast<W>(1) << pos);
+
+            return *this;
         }
 
         [[gnu::always_inline, nodiscard]] constexpr operator bool() const {
-            return (*word_ >> pos_) != T(0);
+            return (*word_ptr >> pos) & 1;
         }
 
         [[gnu::always_inline, nodiscard]] constexpr bool operator~() const {
-            return (*word_ >> pos_) == T(0);
+            return !operator bool();
         }
 
         [[gnu::always_inline]] constexpr void flip() {
-            *word_ ^= T(1) << pos_;
+            *word_ptr ^= static_cast<W>(1) << pos;
         }
 
     private:
-        [[gnu::always_inline]] constexpr reference(T *word, usize pos)
-            : word_{word}, pos_{pos} {}
+        [[gnu::always_inline]] constexpr reference(W *w_ptr, usize p)
+            : word_ptr(w_ptr), pos(p) {}
 
-        T *word_{nullptr};
-        usize pos_{0};
+        W *const word_ptr;
+        const usize pos;
 
         friend class bitset;
     };
 
-    [[gnu::always_inline]] constexpr bool operator[](usize pos) const {
-        return (buf_[pos >> S] >> (pos & M)) != T(0);
+    [[gnu::always_inline, nodiscard]] constexpr reference operator[](usize pos) {
+        return reference(&d[pos / S], pos % S);
     }
 
-    [[gnu::always_inline]] constexpr reference operator[](usize pos) {
-        return reference(&buf_[pos >> S], pos & M);
+    template <typename E, bool D>
+    [[gnu::always_inline]] explicit constexpr bitset(const expr<N, W, D, E> &expr) {
+        if constexpr (D)
+            for (usize i = 0; i < M; ++i) d[i] = expr.word(i);
+        else
+            for (usize i = M; i > 0; --i) d[i - 1] = expr.word(i - 1);
+    }
+
+    template <typename E, bool D>
+    [[gnu::always_inline]] constexpr bitset &operator=(const expr<N, W, D, E> &expr) {
+        if constexpr (D)
+            for (usize i = 0; i < M; ++i) d[i] = expr.word(i);
+        else
+            for (usize i = M; i > 0; --i) d[i - 1] = expr.word(i - 1);
+
+        return *this;
+    }
+
+    [[gnu::always_inline]] constexpr void swap(bitset &other) noexcept {
+        std::swap(d, other.d);
+    }
+
+    template <typename E, bool D>
+    [[gnu::always_inline]] constexpr bitset &operator|=(const expr<N, W, D, E> &expr) & {
+        return *this = *this | expr;
+    }
+
+    template <typename E, bool D>
+    [[gnu::always_inline]] constexpr bitset &operator&=(const expr<N, W, D, E> &expr) & {
+        return *this = *this & expr;
+    }
+
+    template <typename E, bool D>
+    [[gnu::always_inline]] constexpr bitset &operator^=(const expr<N, W, D, E> &expr) & {
+        return *this = *this ^ expr;
+    }
+
+    [[gnu::always_inline]] constexpr bitset &operator<<=(const usize shift) & {
+        return *this = *this << shift;
+    }
+
+    [[gnu::always_inline]] constexpr bitset &operator>>=(const usize shift) & {
+        return *this = *this >> shift;
+    }
+
+    [[gnu::always_inline, nodiscard]] constexpr W word(usize pos) const {
+        return d[pos];
     }
 
     [[gnu::always_inline]] constexpr void set(usize pos) {
-        buf_[pos >> S] |= T(1) << (pos & M);
+        set(pos, true);
     }
 
     [[gnu::always_inline]] constexpr void unset(usize pos) {
-        buf_[pos >> S] &= ~(T(1) << (pos & M));
+        d[pos / S] &= ~(static_cast<W>(1) << (pos % S));
     }
 
     [[gnu::always_inline]] constexpr void flip(usize pos) {
-        buf_[pos >> S] ^= T(1) << (pos & M);
+        d[pos / S] ^= static_cast<W>(1) << (pos % S);
+    }
+
+    [[gnu::always_inline]] constexpr void reset() {
+        std::fill(d.begin(), d.end(), static_cast<W>(0));
     }
 
     [[gnu::always_inline, nodiscard]] constexpr bool any() const {
-        for (usize i = 0; i < B; ++i)
-            if (buf_[i]) return true;
-
-        return false;
+        return std::any_of(d.begin(), d.end(), [](const W w) { return w != 0; });
     }
 
     [[gnu::always_inline, nodiscard]] constexpr bool none() const {
         return !any();
     }
 
-    [[gnu::always_inline]] constexpr void reset() {
-        std::fill(buf_.begin(), buf_.end(), T(0));
-    }
-
-    [[gnu::always_inline, nodiscard]] constexpr usize count() const {
-        usize cnt = 0;
-        for (usize i = 0; i < B; ++i) cnt += popcnt(buf_[i]);
-
-        return cnt;
-    }
-
-    [[gnu::always_inline, nodiscard]] constexpr usize count_intersection(const bitset &rhs) const {
-        return count_op(rhs, std::bit_and{});
-    }
-
-    [[gnu::always_inline, nodiscard]] constexpr usize count_union(const bitset &rhs) const {
-        return count_op(rhs, std::bit_or{});
-    }
-
-    [[gnu::always_inline, nodiscard]] constexpr usize count_symmetric_difference(const bitset &rhs) const {
-        return count_op(rhs, std::bit_xor{});
-    }
-
-    [[gnu::always_inline]] constexpr bitset &operator&=(const bitset &rhs) & {
-        do_op(rhs, std::bit_and{});
-        return *this;
-    }
-
-    [[gnu::always_inline]] constexpr bitset &operator|=(const bitset &rhs) & {
-        do_op(rhs, std::bit_or{});
-        return *this;
-    }
-
-    [[gnu::always_inline]] constexpr bitset &operator^=(const bitset &rhs) & {
-        do_op(rhs, std::bit_xor{});
-        return *this;
-    }
-
-    [[gnu::always_inline]] constexpr bitset &operator<<=(usize shift) & {
-        if (shift >= N) [[unlikely]]
-            reset();
-        else
-            left_shift(shift);
-
-        return *this;
-    }
-
-    [[gnu::always_inline]] constexpr bitset &operator>>=(usize shift) & {
-        if (shift >= N) [[unlikely]]
-            reset();
-        else
-            right_shift(shift);
-
-        return *this;
-    }
-
-    friend std::istream &operator>>(std::istream &is, bitset &b) {
+    [[gnu::always_inline]] friend constexpr std::istream &operator>>(std::istream &is, bitset &b) {
         std::string s;
         s.reserve(N);
 
         is >> s;
 
-        const usize n = std::min(N, s.size());
-        for (usize i = 0, j = n - 1; i < n; ++i, --j) b.set(i, s[j] == '1');
+        const usize n = s.size();
+        for (usize i = 0, j = n; i < n; ++i) b.set(i, s[--j] == '1');
 
         return is;
     }
 
-    [[gnu::always_inline, nodiscard]] friend constexpr bitset operator&(bitset lhs, const bitset &rhs) {
-        return lhs &= rhs;
-    }
-
-    [[gnu::always_inline, nodiscard]] friend constexpr bitset operator|(bitset lhs, const bitset &rhs) {
-        return lhs |= rhs;
-    }
-
-    [[gnu::always_inline, nodiscard]] friend constexpr bitset operator^(bitset lhs, const bitset &rhs) {
-        return lhs ^= rhs;
-    }
-
-    [[gnu::always_inline, nodiscard]] friend constexpr bitset operator<<(bitset lhs, usize shift) {
-        return lhs <<= shift;
-    }
-
-    [[gnu::always_inline, nodiscard]] friend constexpr bitset operator>>(bitset lhs, usize shift) {
-        return lhs >>= shift;
-    }
-
-    [[gnu::always_inline, nodiscard]] friend constexpr bitset operator==(const bitset &lhs, const bitset &rhs) {
-        return lhs.buf_ == rhs.buf_;
-    }
-
 private:
-    std::array<T, B> buf_{};
-
     [[gnu::always_inline]] constexpr void set(usize pos, bool val) {
-        buf_[pos >> S] |= (T(val) << (pos & M));
-    }
-
-    [[gnu::always_inline]] constexpr void left_shift(usize shift) {
-        if (shift == 0) [[unlikely]]
-            return;
-
-        const usize wshift = shift >> S;
-        const usize offset = shift & M;
-
-        if (offset == 0) {
-            for (usize i = B - 1; i >= wshift; --i) buf_[i] = buf_[i - wshift];
-        } else {
-            const usize sub_offset = W - offset;
-            for (usize i = B - 1; i > wshift; --i)
-                buf_[i] = (buf_[i - wshift] << offset) | (buf_[i - wshift - 1] >> sub_offset);
-            buf_[wshift] = buf_[0] << offset;
-        }
-
-        std::fill(buf_.begin(), buf_.begin() + wshift, T(0));
-    }
-
-    [[gnu::always_inline]] constexpr void right_shift(usize shift) {
-        if (shift == 0) [[unlikely]]
-            return;
-
-        const usize wshift = shift >> S;
-        const usize offset = shift & M;
-        const usize limit = B - wshift - 1;
-
-        if (offset == 0) {
-            for (usize i = 0; i <= limit; ++i) buf_[i] = buf_[i + wshift];
-        } else {
-            const usize sub_offset = W - offset;
-            for (usize i = 0; i < limit; ++i)
-                buf_[i] = (buf_[i + wshift] >> offset) | (buf_[i + wshift + 1] << sub_offset);
-            buf_[limit] = buf_[B - 1] >> offset;
-        }
-
-        std::fill(buf_.begin() + limit + 1, buf_.end(), T(0));
-    }
-
-    template <typename Op>
-    [[gnu::always_inline]] constexpr void do_op(const bitset &rhs, Op op) & {
-        for (usize i = 0; i < B; ++i) buf_[i] = op(buf_[i], rhs.buf_[i]);
-    }
-
-    template <typename Op>
-    [[gnu::always_inline, nodiscard]] constexpr usize count_op(const bitset &rhs, Op op) const {
-        usize cnt = 0;
-        for (usize i = 0; i < B; ++i) cnt += popcnt(op(buf_[i], rhs.buf_[i]));
-
-        return cnt;
+        d[pos / S] |= static_cast<W>(val) << (pos % S);
     }
 };
 
