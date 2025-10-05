6437d81791147e8080091a4111b04e8962b2295a
nandhagk <nandhagopi@gmail.com>
2025-10-05 18:05:18 +0530

    fix majority

--- lib/monoids/misra_gries.hpp
+++ lib/monoids/misra_gries.hpp
@@ -1,5 +1,5 @@
-#ifndef LIB_MONOID_MAJORITY_HPP
-#define LIB_MONOID_MAJORITY_HPP 1
+#ifndef LIB_MONOID_MISRA_GRIES_HPP
+#define LIB_MONOID_MISRA_GRIES_HPP 1
 
 #include <algorithm>
 #include <utility>
@@ -8,13 +8,15 @@
 #include <lib/prelude.hpp>
 
 template <typename T, usize K>
-struct monoid_majority {
+struct monoid_misra_gries {
+	static_assert(K >= 2);
+
     using X = std::vector<std::pair<T, u32>>;
     using ValueT = X;
 
-    static constexpr X op(const X &x, const X &y) {
+    static X op(const X &x, const X &y) {
         X z;
-        z.reserve(2 * K);
+        z.reserve(2 * (K - 1));
 
         auto it = x.begin();
         auto ti = y.begin();
@@ -39,24 +41,24 @@ struct monoid_majority {
         for (; it != x.end(); ++it) z.emplace_back(*it);
         for (; ti != y.end(); ++ti) z.emplace_back(*ti);
 
-        if (z.size() > K) {
-            for (auto &&[t, c] : z) --c;
-            z.erase(std::remove_if(z.begin(), z.end(), [&](const auto &p) { return p.second <= 0; }), z.end());
-            z.shrink_to_fit();
+		while (z.size() >= K) {
+			const u32 c = std::min_element(z.begin(), z.end(), [](const auto &a, const auto &b) { return a.second < b.second; })->second;
+            for (auto &p : z) p.second -= c;
+			z.erase(std::remove_if(z.begin(), z.end(), [&](const auto &p) { return p.second <= 0; }), z.end());
         }
 
         return z;
     }
 
-    static constexpr X unit() {
+    static X unit() {
         return X{};
     }
 
-    static constexpr X from_element(const T &t) {
+    static X from_element(const T &t) {
         return X{{t, 1}};
     }
 
     static constexpr bool commutative = true;
 };
 
-#endif // LIB_MONOID_MAJORITY_HPP
+#endif // LIB_MONOID_MISRA_GRIES_HPP
